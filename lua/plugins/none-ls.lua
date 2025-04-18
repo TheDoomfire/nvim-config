ada2082f810f162aad34a012a5ea7d83f779e257
Emma <emmathefemma@gmail.com>
2025-04-18 22:15:04 +0200

    trying code_actios + hovers

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,35 +1,47 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				--null_ls.builtins.formatting.djlint,
-				require("none-ls.diagnostics.eslint_d"),
-				require("none-ls.diagnostics.ruff"),
-				require("none-ls.formatting.ruff"),
-				--null_ls.builtins.completion.spell,
-			},
-			-- Set up format on save
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ bufnr = bufnr })
-						end,
-					})
-				end
-			end,
-			debug = true,
-		})
-		-- Fallback if format on save is not working.
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    null_ls.setup({
+      sources = {
+        -- FORMATTING
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        require("none-ls.formatting.ruff"),
+        --null_ls.builtins.formatting.djlint,
+
+        -- DIAGNOSTICS
+        require("none-ls.diagnostics.eslint_d"),
+        require("none-ls.diagnostics.ruff"),
+
+        -- CODE ACTIONS
+        --null_ls.builtins.code_actions.eslint_d, -- Auto-fix ESLint errors
+        null_ls.builtins.code_actions.gitsigns, -- Git conflict resolution
+
+        -- HOVER
+        null_ls.builtins.hover.dictionary, -- Word definitions
+        null_ls.builtins.hover.printenv, -- Environment variable info
+
+        --null_ls.builtins.completion.spell,
+      },
+      -- Set up format on save
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+          })
+        end
+      end,
+      debug = true,
+    })
+    -- Fallback if format on save is not working.
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
