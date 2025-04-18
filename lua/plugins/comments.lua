0cecddba3a73fb1a0dab014bd47728a045d22515
Emma <emmathefemma@gmail.com>
2025-04-19 00:09:14 +0200

    comments

---
+++ lua/plugins/comments.lua
@@ -0,0 +1,6 @@
+return {
+	"tpope/vim-commentary",
+}
+
+-- this is to make comments easier.
+--   Like this: gcc, 3ggc, gc, gcap.
--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,30 +1,21 @@
 return {
 	"folke/snacks.nvim",
 	--config = function()
-		--require("snacks").setup({
-			priority = 1000,
-			lazy = false,
-			--@type snacks.Config
-			opts = {
-    bigfile = { enabled = true },
-				dashboard = {
-					enabled = true,
-					sections = {
-						{ section = "header" },
-						{ section = "keys", gap = 1, padding = 1 },
-						{ section = "startup" },
-						{
-							section = "terminal",
-          -- TODO: Make terminal work with linux too.
-                            cmd = "git --version",
-							random = 10,
-							pane = 2,
-							indent = 4,
-							height = 10,
-						},
-					},
-				},
+	--require("snacks").setup({
+	priority = 1000,
+	lazy = false,
+	--@type snacks.Config
+	opts = {
+		bigfile = { enabled = true },
+		dashboard = {
+			enabled = true,
+			sections = {
+				{ section = "header" },
+				{ section = "keys", gap = 1, padding = 1 },
+				{ section = "startup" },
 			},
-		--})
+		},
+	},
+	--})
 	--end,
 }
