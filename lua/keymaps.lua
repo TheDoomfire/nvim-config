bf82e5352fd0b6816e8d6eb2ddd8e4220183e688
Emma <emmathefemma@gmail.com>
2025-04-20 03:52:03 +0200

    New keys + visual multiline

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,18 @@
 --vim.keymap.set("v", "<leader>y", '"+y', { noremap = true })
 --vim.keymap.set("n", "<leader>p", '"+p', { noremap = true })
-vim.keymap.set('n', '<C-a>', 'ggVG', { noremap = true, desc = 'Select all' })
+
+-- Select all with Alt+a.
+vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true, desc = "Select all" })
+
+-- Move lines with Alt+Up/Down
+vim.keymap.set("n", "<A-Up>", ":m .-2<CR>==", { desc = "Move line up" })
+vim.keymap.set("n", "<A-Down>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+
+-- Copy lines with Alt+Shift+Up/Down
+vim.keymap.set("n", "<A-S-Up>", ":copy .-1<CR>", { desc = "Copy line up" })
+vim.keymap.set("n", "<A-S-Down>", ":copy .<CR>", { desc = "Copy line down" })
+vim.keymap.set("v", "<A-S-Down>", ":copy '><CR>gv", { desc = "Copy selection down" })
+vim.keymap.set("v", "<A-S-Up>", ":copy '<-1<CR>gv", { desc = "Copy selection up" })
+vim.keymap.set("v", "<A-S-Down>", ":copy '><CR>gv", { desc = "Copy selection down" })
---
+++ lua/plugins/visual-multi.lua
@@ -0,0 +1,6 @@
+return {
+  "mg979/vim-visual-multi",
+}
+-- To edit multiple lines at once.
+--  I wanna use it like vscode "CTRL+L" to select all exact word to edit them all.
+--  Like: CTRL+L, CTRL+D, Tab / Tab+Shift
