ec494b456246f0f9c947d709d5e323fc669c4a9a
Albin <albinxd001@gmail.com>
2025-05-24 22:48:23 +0200

    misc

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -31,7 +31,8 @@ return {
       },
       -- Set up format on save
       on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
+        -- if client.supports_method("textDocument/formatting") then
+        if client:supports_method("textDocument/formatting") then
           vim.api.nvim_create_autocmd("BufWritePre", {
             buffer = bufnr,
             callback = function()
